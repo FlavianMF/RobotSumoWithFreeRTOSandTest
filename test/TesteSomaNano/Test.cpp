44dbc70d836485e8ccef7c22e92be2d58f90ac46
FlavianOMEGA <flavianmelquiades@gmail.com>
2020-06-07 18:39:50 -0300

    Não ta rodando os testes na minha maquina, teste no Travis

---
+++ test/TesteSomaNano/Test.cpp
@@ -0,0 +1,17 @@
+#include <unity.h>
+#include <SOMA.h>
+#include <Arduino.h>
+
+void test_SOMA(void){
+    TEST_ASSERT_EQUAL_INT(18, soma(12, 6));
+}
+
+void setup(){
+	UNITY_BEGIN();
+    RUN_TEST(test_SOMA);
+    UNITY_END();
+}
+
+void loop(){
+
+}

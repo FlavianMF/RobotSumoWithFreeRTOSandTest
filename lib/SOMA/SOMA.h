15245ea49426c2c6b120189938ca17bae30d4a72
FlavianOMEGA <flavianmelquiades@gmail.com>
2020-06-08 07:47:42 -0300

    Zerando Tudo Pra começar de fato

--- lib/SOMA/SOMA.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOMA_H
-#define SOMA_H
-
-int soma(int elementA, int elementB){
-    return elementA + elementB;
-}
-
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <Arduino.h>
-#include <SOMA.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  //Teste Travis
-  //More Test
-  int elementC = soma(12, 6);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }

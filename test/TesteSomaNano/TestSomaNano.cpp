1a2eeb1e7e2e0d4e2234d4c03790ed676a029336
FlavianOMEGA <flavianmelquiades@gmail.com>
2020-06-07 21:58:14 -0300

    Teste somente em "native"

--- test/TesteSomaNano/TestSomaNano.cpp
+++ test/TesteSomaNano/TestSomaNano.cpp
@@ -1,17 +1,18 @@
+#include <Arduino.h>
 #include <unity.h>
 #include <SOMA.h>
-#include <Arduino.h>
 
 void test_SOMA(void){
     TEST_ASSERT_EQUAL_INT(18, soma(12, 6));
 }
 
 void setup(){
-	UNITY_BEGIN();
+    UNITY_BEGIN();
     RUN_TEST(test_SOMA);
     UNITY_END();
 }
 
 void loop(){
+
 
 }
